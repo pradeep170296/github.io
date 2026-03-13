0722c958d13035d00efc4d4577d661c569a309d0
pradeep170296 <pradeep.murugesan@jlk-tech.co.in>
2026-03-13 12:00:24 +0530

    Create Users.js

---
+++ Users.js
@@ -0,0 +1,7 @@
+const users = [
+
+    {username:"admin", password:"admin123", role:"admin"},
+    {username:"it", password:"it123", role:"user"},
+    {username:"support", password:"1234", role:"user"}
+
+];
