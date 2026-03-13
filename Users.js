0251e0897bb8ae99115b984de74dfb34cc5d8b9d
pradeep170296 <pradeep.murugesan@jlk-tech.co.in>
2026-03-13 15:28:34 +0530

    Delete Users.js

--- Users.js
+++
@@ -1,7 +0,0 @@
-const users = [
-
-    {username:"admin", password:"admin123", role:"admin"},
-    {username:"it", password:"it123", role:"user"},
-    {username:"support", password:"1234", role:"user"}
-
-];
