b8fd0e140614e051b070f191d20e5d16c35b8b80
pradeep170296 <pradeep.murugesan@jlk-tech.co.in>
2026-03-13 15:31:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,44 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const fs = require("fs");
 
 app.use(express.static("public"));
+app.use(express.json());
 
-io.on("connection", (socket) => {
-    console.log("User connected");
+const users = JSON.parse(fs.readFileSync("users.json"));
 
-    socket.on("remote-command", (data) => {
-        socket.broadcast.emit("execute", data);
+app.post("/login", (req, res) => {
+
+    const { username, password } = req.body;
+
+    const user = users.find(
+        u => u.username === username && u.password === password
+    );
+
+    if (user)
+        res.json({ status: "ok" });
+    else
+        res.json({ status: "fail" });
+
+});
+
+io.on("connection", socket => {
+
+    socket.on("offer", data => {
+        socket.broadcast.emit("offer", data);
+    });
+
+    socket.on("answer", data => {
+        socket.broadcast.emit("answer", data);
+    });
+
+    socket.on("ice", data => {
+        socket.broadcast.emit("ice", data);
     });
 
 });
 
-http.listen(3000, () => {
-    console.log("Server running on 3000");
-});
+http.listen(3000, () =>
+    console.log("Server running 3000")
+);
