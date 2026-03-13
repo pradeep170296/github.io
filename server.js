3ea765602d84e83042007ff3048a693778ed4774
pradeep170296 <pradeep.murugesan@jlk-tech.co.in>
2026-03-13 15:16:18 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("remote-command", (data) => {
+        socket.broadcast.emit("execute", data);
+    });
+
+});
+
+http.listen(3000, () => {
+    console.log("Server running on 3000");
+});
